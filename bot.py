d1f7f87bbc3bb1c1d956f0a35284379a93d50fb3
S1za31 <mishaildarradik15@gmail.com>
2026-05-23 19:24:24 +0300

    create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import telebot
+import os
+
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot("8943965379:AAFe2Zm_ZCiPDV0HTTxAp31JMC04x40Rk1E")
+
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+# Обработчик команды '/mem'
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    images = os.listdir('images')
+    for image in images:
+        with open(f'images/{image}', 'rb') as f:  
+            bot.send_photo(message.chat.id, f)
+
+# Обработчик команды '/eco'
+@bot.message_handler(commands=['eco'])
+def send_eco(message):
+    bot.reply_to(message, "Экология — наука о взаимоотношениях живых организмов между собой и с окружающей их средой. Она изучает организацию и функционирование биологических систем различных уровней: популяций, биоценозов (сообществ), экосистем и биосферы в целом.")
+
+
+@bot.message_handler(commands=['nature'])
+def send_nature(message):
+    bot.reply_to(message, "Природа — это материальный мир Вселенной в его естественном состоянии, не изменённый деятельностью человека. Это всё, что окружает человека и не создано его руками.")
+
+
+# Запуск бота
+bot.polling()
